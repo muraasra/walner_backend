214bcbe185bdd1e5dc14e7f0f8b0754fd05378d1
muraasra <wilfriedtayou6@gmail.com>
2025-05-17 17:59:33 +0200

    Automate makemigrations + migrate in build

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-set -o errexit       # exit on error
+set -o errexit
+
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+# Crée les fichiers de migration si nécessaire
+python manage.py makemigrations --no-input
+
+# Applique toutes les migrations
+python manage.py migrate --no-input
