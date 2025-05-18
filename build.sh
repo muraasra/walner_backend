99ffadbe674ed21d212facfc4d2debbf6418af72
muraasra <wilfriedtayou6@gmail.com>
2025-05-18 04:39:40 +0200

    build

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ pip install -r requirements.txt
 python3 manage.py collectstatic --no-input
 
 # Crée les fichiers de migration si nécessaire
-python3 manage.py makemigrations --no-input
+python3 manage.py makemigrations  --no-input
 
 # Applique toutes les migrations
-python3 manage.py migrate --no-input
+python3 manage.py migrate --fake-initial  --no-input
